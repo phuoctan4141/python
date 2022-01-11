f895ea665ddef1d7a232af5848a9e0721a9f9e7f
phuoctan4141 <phuoctan4141@hotmail.com>
2022-01-12 00:45:26 +0700

    Create a_window.py

---
+++ pythonGUIs/Basic PyQt5 Features/My first Application/a_window.py
@@ -0,0 +1,21 @@
+import sys
+from PyQt5.QtCore import QSize, Qt
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+
+
+# Subclass QMainWindow to customize your application's main window
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("My First App")
+        button = QPushButton("Press Me!")
+        # Setting the size on the window
+        self.setFixedSize(QSize(400, 300))
+        # Set the central widget of the Window.
+        self.setCentralWidget(button)
+
+
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+app.exec_()
